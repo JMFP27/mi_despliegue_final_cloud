95adebf6efa2d390c6ea8c668c6b4d2043e18016
Juan Manuel Fajardo <juan.fajardo@usantoto.edu.co>
2025-11-26 10:57:51 -0500

    Update error.ts

--- pds006-frameworks-20252/src/core/service/error.ts
+++ pds006-frameworks-20252/src/core/service/error.ts
@@ -1,9 +1,15 @@
-import { ErrorBase } from "../../utils"; // Alias corregido: sube un nivel de 'service' a 'core' y entra a 'utils'
+import { ErrorBase } from "../utils"; // Corregido: La ruta relativa correcta desde 'src/core/service' a 'src/core/utils' es '../utils'.
 
 type ServiceErrorName =
   | 'DEVICE_NOT_FOUND'
 
-export class ServiceError extends ErrorBase<ServiceErrorName> {}
+// Se añade el constructor explícito para resolver el error TS2554 ("Expected 0 arguments, but got 2").
+// Esto permite que la clase ServiceError (que extiende ErrorBase) acepte los argumentos 'name' y 'message'.
+export class ServiceError extends ErrorBase<ServiceErrorName> {
+  constructor(name: ServiceErrorName, message: string) {
+    super(name, message);
+  }
+}
 
 export const SERVICE_ERRORS = {
   DeviceNotFound: new ServiceError('DEVICE_NOT_FOUND', "Dispositivo no encontrado")
